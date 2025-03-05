cc1fc4d2880bc350599900b0da68593368fe793d
Sajeeb <sajeeb2186@gmail.com>
2025-03-05 17:02:02 +0600

    crud method tested

--- app/api/topics/route.js
+++ app/api/topics/route.js
@@ -14,3 +14,22 @@ export async function GET() {
   const topics = await Topic.find();
   return NextResponse.json({ topics });
 }
+
+export async function DELETE(request) {
+  const id = request.nextUrl.searchParams.get("id");
+
+  await connectMongoDB();
+
+  await Topic.findByIdAndDelete(id);
+  return NextResponse.json({ message: "topic deleted" }, { status: 200 });
+}
+
+export async function PUT(request, { params }) {
+  const { id } = params;
+
+  const { newTitle: title, newDescription: description } = await request.json();
+
+  await connectMongoDB();
+  await Topic.findByIdAndUpdate(id, { title, description });
+  return NextResponse.json({ message: "Topic update" }, { status: 200 });
+}
