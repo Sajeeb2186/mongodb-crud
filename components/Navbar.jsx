c8709814f2c7845c5e49bfca3f136205ab85e020
Sajeeb <sajeeb2186@gmail.com>
2025-04-16 23:52:45 +0600

    s

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import LoginButton from "./LoginButton";
 
 
 export default function Navbar() {
@@ -8,7 +9,10 @@ export default function Navbar() {
         <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
 
             <Link className="text-white font-bold" href={'/'}>NextCrud</Link>
+            <LoginButton className="bg-white text-red-700">signIn</LoginButton>
             <Link className="bg-white p-2 rounded" href={'/addTopic'}>Add Topic</Link>
+
+            
 
 
 
